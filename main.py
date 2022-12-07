f356f9d021516c23637b6375e5a4a1773c34f869
Kuj1 <the.same.dentist@gmail.com>
2022-12-07 17:14:13 +0300

    refactored code and added new var to parse without posting

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from pyfiglet import Figlet
 
 from scripts.get_books import main, announce_result
 
-
 if __name__ == '__main__':
     title = Figlet(font='cosmic')
     print(title.renderText('K-Pars\nBooks'))
     while True:
-        print('1) Start scanning data')
-        print('2) Show parsed results\n')
+        print('1) Start scanning data and posting')
+        print('2) Start scanning data without posting')
+        print('3) Show parsed results\n')
+        # print('3) Start scanning data without posting\n')
         enter_decision = int(input('Choose option and enter number:\n-> '))
         if enter_decision == 1:
             print('\n[Receiving data]\n')
@@ -22,6 +23,14 @@ if __name__ == '__main__':
                 print(stop - start)
                 asyncio.run(announce_result())
         elif enter_decision == 2:
+            print('\n[Receiving data]\n')
+            while True:
+                start = time.time()
+                main()
+                stop = time.time()
+                print(stop - start)
+                asyncio.run(announce_result(not_to_post=True))
+        elif enter_decision == 3:
             try:
                 with open('result_files/result_data.txt', 'r', encoding="utf-8") as res:
                     for i in res:
